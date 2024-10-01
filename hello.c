85db2bf3ad401aa07217b49d7185a772f7ec3805
Steven Nguyen <121068731+nguyensd0317@users.noreply.github.com>
2024-10-01 19:46:17 +0000

    yippee

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+  char hello3[] = "I'm Steven";
+  puts(hello);
+  puts(hello2);
+  puts(hello3);
+
+  return 0;
+}
